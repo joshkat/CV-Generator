bd0d1da8b7af927081db50f130c280be2616b3b6
Josh <joshua02k@gmail.com>
2023-06-28 00:44:25 -0400

    moved exp state to App

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,16 @@ import GeneralInfo from "./components/inputs/GeneralInfo.jsx";
 import Experience from "./components/inputs/Experience.jsx";
 import Education from "./components/inputs/Education.jsx";
 
+import ExperiencePreview from "./components/cv-view/ExperiencePreview.jsx";
+
+import { useState } from "react";
+
 function App() {
+  //experience state
+  const [expNumber, setExpNum] = useState(2);
+  const [expArray, setExpArr] = useState([
+    { number: 1, title: "", location: "", description: "" },
+  ]);
   return (
     <>
       <form className="inputForm">
@@ -16,14 +25,19 @@ function App() {
         </fieldset>
         <fieldset>
           <legend>Experience</legend>
-          <Experience />
+          <Experience
+            expNumber={expNumber}
+            setExpNum={setExpNum}
+            expArray={expArray}
+            setExpArr={setExpArr}
+          />
         </fieldset>
       </form>
       <div className="cv-display">
-        left side will be input, right side will display a prettier version of
-        inputs
-        <div className="title1"></div>
-        <div className="title2"></div>
+        {/* The cv-views are added within their respective input components */}
+        <div className="general-info">general info</div>
+        <div className="education">education</div>
+        <ExperiencePreview experienceArray={expArray} />
       </div>
     </>
   );
