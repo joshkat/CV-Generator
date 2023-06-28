08cc525a8b489283972494f886e422ae4a5f51bb
Josh <joshua02k@gmail.com>
2023-06-28 15:56:58 -0400

    added education preview & styles

--- src/components/cv-view/EducationPreview.jsx
+++ src/components/cv-view/EducationPreview.jsx
@@ -1,13 +1,24 @@
 import PropTypes from "prop-types";
+import "../../styles/Education.css";
 
 export default function EducationPreview({ education }) {
   return (
     <>
-      <span>{education.schoolName}</span>
-      <span>{education.city}</span>
-      <span>{education.major}</span>
+      <h3 className="education-title">Education</h3>
 
-      <div>{education.date}</div>
+      <div className="section-line"></div>
+      <div className="education-topline">
+        <div>
+          <span>{education.schoolName || "School Name"}&ensp;</span>
+          <span className="gpa">GPA {education.gpa}&ensp;</span>
+        </div>
+        <span>{education.city || "City"}&ensp;</span>
+      </div>
+
+      <em className="education-bottomline">
+        <span>{education.major || "Major"}&ensp;</span>
+        <span>{education.date || "date"}&ensp;</span>
+      </em>
     </>
   );
 }
