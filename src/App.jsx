3225b0e3c7fdd9de919844839d445e8732cd4573
Josh <joshua02k@gmail.com>
2023-06-28 12:26:08 -0400

    moved form into seperate component

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,11 @@
-import GeneralInfo from "./components/inputs/GeneralInfo.jsx";
-import Experience from "./components/inputs/Experience.jsx";
-import Education from "./components/inputs/Education.jsx";
-
-import GeneralInfoPreview from "./components/cv-view/GeneralInfoPreview.jsx";
-import EducationPreview from "./components/cv-view/EducationPreview.jsx";
-import ExperiencePreview from "./components/cv-view/ExperiencePreview.jsx";
-
-import { useState } from "react";
+import MainSection from "./components/MainSection.jsx";
 
 function App() {
-  //general info state
-  const [generalInfo, setGeneralInfo] = useState({
-    firstName: "Your Name",
-    lastName: "",
-    address: "",
-    phoneNumber: "",
-    email: "",
-    infoDescription: "",
-  });
-
-  //education state
-  const [education, setEducation] = useState({
-    schoolName: "",
-    gpa: "",
-    city: "",
-    major: "",
-    date: "",
-  });
-
-  //experience state
-  const [expNumber, setExpNum] = useState(2);
-  const [expArray, setExpArr] = useState([
-    { number: 1, title: "", location: "", description: "" },
-  ]);
   return (
     <>
-      <form className="inputForm">
-        <fieldset>
-          <legend>General Information</legend>
-          <GeneralInfo setGeneralInfo={setGeneralInfo} />
-        </fieldset>
-        <fieldset>
-          <legend>Education</legend>
-          <Education setEducation={setEducation} />
-        </fieldset>
-        <fieldset>
-          <legend>Experience</legend>
-          <Experience
-            expNumber={expNumber}
-            setExpNum={setExpNum}
-            expArray={expArray}
-            setExpArr={setExpArr}
-          />
-        </fieldset>
-      </form>
-      <div className="cv-display">
-        {/* The cv-views are added within their respective input components */}
-        <GeneralInfoPreview generalInfoObj={generalInfo} />
-        <EducationPreview education={education} />
-        <ExperiencePreview experienceArray={expArray} />
+      <header>Hello</header>
+      <div className="main-wrapper">
+        <MainSection></MainSection>
       </div>
     </>
   );
---
+++ src/components/MainSection.jsx
@@ -0,0 +1,68 @@
+import GeneralInfo from "./inputs/GeneralInfo.jsx";
+import Experience from "./inputs/Experience.jsx";
+import Education from "./inputs/Education.jsx";
+
+import GeneralInfoPreview from "./cv-view/GeneralInfoPreview.jsx";
+import EducationPreview from "./cv-view/EducationPreview.jsx";
+import ExperiencePreview from "./cv-view/ExperiencePreview.jsx";
+
+import "../styles/MainSection.css";
+
+import { useState } from "react";
+
+export default function MainSection() {
+  //general info state
+  const [generalInfo, setGeneralInfo] = useState({
+    firstName: "Your Name",
+    lastName: "",
+    address: "",
+    phoneNumber: "",
+    email: "",
+    infoDescription: "",
+  });
+
+  //education state
+  const [education, setEducation] = useState({
+    schoolName: "",
+    gpa: "",
+    city: "",
+    major: "",
+    date: "",
+  });
+
+  //experience state
+  const [expNumber, setExpNum] = useState(2);
+  const [expArray, setExpArr] = useState([
+    { number: 1, title: "", location: "", description: "" },
+  ]);
+
+  return (
+    <>
+      <form className="inputForm">
+        <fieldset>
+          <legend>General Information</legend>
+          <GeneralInfo setGeneralInfo={setGeneralInfo} />
+        </fieldset>
+        <fieldset>
+          <legend>Education</legend>
+          <Education setEducation={setEducation} />
+        </fieldset>
+        <fieldset>
+          <legend>Experience</legend>
+          <Experience
+            expNumber={expNumber}
+            setExpNum={setExpNum}
+            expArray={expArray}
+            setExpArr={setExpArr}
+          />
+        </fieldset>
+      </form>
+      <div className="cv-display">
+        {/* The cv-views are added within their respective input components */}
+        <GeneralInfoPreview generalInfoObj={generalInfo} />
+        <EducationPreview education={education} />
+        <ExperiencePreview experienceArray={expArray} />
+      </div>
+    </>
+  );
+}
